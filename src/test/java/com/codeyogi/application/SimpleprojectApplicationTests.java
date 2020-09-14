b11eb5ac30987f64e738bc64c239472cf26ebc34
b.shubham <b.shubham@aitglobalinc.com>
2020-09-14 20:59:59 +0530

    Initial Commit

---
+++ src/test/java/com/codeyogi/application/SimpleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeyogi.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
